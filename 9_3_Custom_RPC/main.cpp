fe43abe2b15a36efe505dbc68132d46be95b4b01
uweiLIn <Lin33113311@gmail.com>
2021-04-21 17:56:22 +0800

    add all

--- 9_3_Custom_RPC/main.cpp
+++ 9_3_Custom_RPC/main.cpp
@@ -12,10 +12,14 @@ BufferedSerial pc(USBTX, USBRX);
 void LEDControl(Arguments *in, Reply *out);
 RPCFunction rpcLED(&LEDControl, "LEDControl");
 double x, y;
+Thread t; 
+void thread();
 
 int main() {
     //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
 
+
+    t.start(thread);
     // receive commands, and send back the responses
     char buf[256], outbuf[256];
 
@@ -49,14 +53,53 @@ void LEDControl (Arguments *in, Reply *out)   {
     // Have code here to call another RPC function to wake up specific led or close it.
     char buffer[200], outbuf[256];
     char strings[20];
-    int led = x;
-    int on = y;
-    sprintf(strings, "/myled%d/write %d", led, on);
-    strcpy(buffer, strings);
-    RPC::call(buffer, outbuf);
+    
+
+    //sprintf(strings, "/myled%d/write %d", led, on);
+    //strcpy(buffer, strings);
+    //RPC::call(buffer, outbuf);
     if (success) {
         out->putData(buffer);
     } else {
         out->putData("Failed to execute LED control.");
     }
+}
+
+void thread(){
+    char buffer[200], outbuf[256];
+    char strings[20];
+    int led1, led2;
+    while (1)
+    {
+       if (x == 1) {
+           if (led1 == 1) {
+               led1 = 0;
+               sprintf(strings, "/myled1/write 0");
+               strcpy(buffer, strings);
+               RPC::call(buffer, outbuf);
+           } else {
+               led1 = 1;
+               sprintf(strings, "/myled1/write 1");
+               strcpy(buffer, strings);
+               RPC::call(buffer, outbuf);
+           }
+       }
+       if (y == 1) {
+           if (led2 == 1) {
+               led2 = 0;
+               sprintf(strings, "/myled3/write 0");
+               strcpy(buffer, strings);
+               RPC::call(buffer, outbuf);
+           } else {
+               led2 = 1;
+               sprintf(strings, "/myled3/write 1");
+               strcpy(buffer, strings);
+               RPC::call(buffer, outbuf);
+           }
+       }
+       ThisThread::sleep_for(100ms);
+
+    }
+    
+    
 }
